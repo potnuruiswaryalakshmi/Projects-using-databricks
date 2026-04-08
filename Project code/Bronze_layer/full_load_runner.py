1279fc725ebceff5a80914a6154e04db287f4be6
Potnuru Iswarya Lakshmi <96614904+potnuruiswaryalakshmi@users.noreply.github.com>
2026-04-08 11:52:06 +0000

    reformatted

--- Project code/Bronze_layer/full_load_runner.py
+++ Project code/Bronze_layer/full_load_runner.py
@@ -3,10 +3,14 @@
 
 # COMMAND ----------
 
-loading_bronze = LoadingBronzeData(spark,full_load_file_path,mode = 'overwrite',file_path_type="parent")
+loading_bronze = LoadingBronzeData(
+    spark, full_load_file_path, mode="overwrite", file_path_type="parent"
+)
 loading_bronze.load_files_convert_tables()
 
 # COMMAND ----------
 
-loading_bronze = LoadingBronzeData(spark,full_load_file_path,mode = 'overwrite',file_path_type="child")
+loading_bronze = LoadingBronzeData(
+    spark, full_load_file_path, mode="overwrite", file_path_type="child"
+)
 loading_bronze.load_files_convert_tables()
--- Project code/Bronze_layer/incremental_load_runner.py
+++ Project code/Bronze_layer/incremental_load_runner.py
@@ -3,10 +3,14 @@
 
 # COMMAND ----------
 
-loading_bronze = LoadingBronzeData(spark,incremental_load_file_path,mode = 'append',file_path_type="parent")
+loading_bronze = LoadingBronzeData(
+    spark, incremental_load_file_path, mode="append", file_path_type="parent"
+)
 loading_bronze.load_files_convert_tables()
 
 # COMMAND ----------
 
-loading_bronze = LoadingBronzeData(spark,incremental_load_file_path,mode = 'append',file_path_type="child")
+loading_bronze = LoadingBronzeData(
+    spark, incremental_load_file_path, mode="append", file_path_type="child"
+)
 loading_bronze.load_files_convert_tables()
