b7ddf27d2f8bcaf3c3afc877057d626a6cb80e38
potnuruiswaryalakshmi0412@gmail.com <potnuruiswaryalakshmi0412@gmail.com>
2026-04-08 11:11:31 +0000

    changes of bronze layer is completed

---
+++ Project code/Bronze_layer/full_load.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %run ./fmcg_Bronze
+
+# COMMAND ----------
+
+loading_bronze = LoadingBronzeData(spark,full_load_file_path,mode = 'overwrite',file_path_type="parent")
+loading_bronze.load_files_convert_tables()
+
+# COMMAND ----------
+
+loading_bronze = LoadingBronzeData(spark,full_load_file_path,mode = 'overwrite',file_path_type="child")
+loading_bronze.load_files_convert_tables()
---
+++ Project code/Bronze_layer/incremental_load.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %run ./fmcg_Bronze
+
+# COMMAND ----------
+
+loading_bronze = LoadingBronzeData(spark,incremental_load_file_path,mode = 'append',file_path_type="parent")
+loading_bronze.load_files_convert_tables()
+
+# COMMAND ----------
+
+loading_bronze = LoadingBronzeData(spark,incremental_load_file_path,mode = 'append',file_path_type="child")
+loading_bronze.load_files_convert_tables()
